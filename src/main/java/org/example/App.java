7e0dd677f6d6c5f6b6cf9c22743fbc9b89c3d937
nicolasdcubillos <79682493+nicolasdcubillos@users.noreply.github.com>
2021-03-01 23:20:03 -0500

    Prueba1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,10 @@ package org.example;
 public class App 
 {
     public static void main( String[] args ) {
-
         System.out.println( "Hello World!" );
+        System.out.println("Hola mundo 123");
+
+
+        System.out.println();
     }
 }
