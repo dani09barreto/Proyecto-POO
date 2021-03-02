d5a021673f91a8aba2566cafc114991407c81461
dani09barreto <73509586+dani09barreto@users.noreply.github.com>
2021-03-02 11:55:42 -0500

    se borraron todas las branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,9 +9,5 @@ public class App
     public static void main( String[] args ) {
 
         System.out.println( "Hello World!" );
-        System.out.println("hola");
-        System.out.println("esto es un pueba linda");
-        System.out.println("me gusta tu prueba");
-        System.out.println("vamola");
     }
 }
