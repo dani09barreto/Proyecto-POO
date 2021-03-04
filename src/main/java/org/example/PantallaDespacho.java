e21368cb191d998254ca433bebead21f18e639ac
Santiago-Xave <73509100+Santiago-Xave@users.noreply.github.com>
2021-03-03 19:28:20 -0500

    Clases y sus atributos
    agregue todas las clases junto con sus atributos, y sus asociaciones .

--- src/main/java/org/example/PantallaDespacho.java
+++ src/main/java/org/example/PantallaDespacho.java
@@ -4,6 +4,7 @@ public class PantallaDespacho {
     private ControlDespacho controlDespacho = new ControlDespacho();
 
     public static void main(String[] args) {
+        System.out.println("hola mkos");
 
 
     }
