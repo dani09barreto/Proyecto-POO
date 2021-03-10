80df8eaf41325316c3cf5a9ea40bb0d0b775f8e7
dani09barreto <73509586+dani09barreto@users.noreply.github.com>
2021-03-09 21:31:57 -0500

    cree el menu

--- src/main/java/org/example/View/PantallaDespacho.java
+++ src/main/java/org/example/View/PantallaDespacho.java
@@ -2,6 +2,8 @@ package org.example.View;
 
 import org.example.Control.ControlDespacho;
 
+import java.util.Scanner;
+
 public class PantallaDespacho {
 
     private ControlDespacho centralDespacho = new ControlDespacho();
@@ -9,13 +11,25 @@ public class PantallaDespacho {
     public static void main(String[] args) {
 
         PantallaDespacho Pantalla = new PantallaDespacho();
-        int opcion = 1;
-        System.out.println("hola mkos");
-        System.out.println("prueba branch");
-        System.out.println(" MENU ");
-        System.out.println("1. inserte producto");
-        System.out.println("2. poner precio");
-
+        int opcion = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n______Oficina de una agencia de reparto de Productos______\n");
+        System.out.println("1.  Ver Lista de Productos");
+        System.out.println("2.  Insertar Productos");
+        System.out.println("3.  Modificar Productos");
+        System.out.println("4.  Eliminar Productos");
+        System.out.println("5.  Ver listado de Clientes registrados en el sistema");
+        System.out.println("6.  Insertar Cliente");
+        System.out.println("7.  Modificar datos de Cliente");
+        System.out.println("8.  Eliminar un Cliente");
+        System.out.println("9.  Realizar el Pedido de un Producto");
+        System.out.println("10. Modificar un Pedido de un Producto");
+        System.out.println("11. Eliminar un Pedido de un Producto");
+        System.out.println("12. Ver listado de Pedidos existentes");
+        System.out.println("13. Ver listado de Pedidos existentes de Producto y fecha específica");
+        System.out.println("0. Salir");
+        System.out.print("Opcion: ");
+        in.nextInt();
         switch (opcion) {
             case 1:
                 Pantalla.centralDespacho.getGestionProductos().VerListadoDeProductos();
