098c2f141bf30ec21210b960697ea43ac21e6497
nicolasdcubillos <79682493+nicolasdcubillos@users.noreply.github.com>
2021-03-11 19:39:22 -0500

    Arregle el metodo de verListadoClientes, ya funciona bien.

--- src/main/java/org/example/Control/GestionCliente.java
+++ src/main/java/org/example/Control/GestionCliente.java
@@ -12,41 +12,43 @@ public class GestionCliente {
         return listaClientes;
     }
 
-    public  Cliente existeCliente( long identificacion){
-        for (Cliente cltemp: listaClientes){
-            if (cltemp.getCedula() == identificacion){
+    public Cliente existeCliente(long identificacion) {
+        for (Cliente cltemp : listaClientes) {
+            if (cltemp.getCedula() == identificacion) {
                 return cltemp;
             }
         }
         return null;
     }
-    public void eliminarCliente (long identificacion){
+
+    public void eliminarCliente(long identificacion) {
 
         boolean ver = true;
         char confirmacion = 0;
-        for (Cliente cli: listaClientes){
+        for (Cliente cli : listaClientes) {
             ver = false;
             System.out.println("\t Desea eliminar el cliente? \nS = si \nN = no");
             Scanner in = new Scanner(System.in);
             confirmacion = in.next().charAt(0);
 
-            if (confirmacion == 'S'){
+            if (confirmacion == 'S') {
                 listaClientes.remove(cli);
-                System.out.println("\tEl documento del cliente elimado es: "+ cli.getCedula());
+                System.out.println("\tEl documento del cliente elimado es: " + cli.getCedula());
                 System.out.println("\tCLIENTE ELIMINADO CON EXITO");
                 break;
             }
         }
-        if(ver && confirmacion != 'N')
+        if (ver && confirmacion != 'N')
             System.out.println("\t PRODUCTO NO ENCONTRADO");
     }
 
-    public void verListadoClientes(){
-        if (this.listaClientes.size() != 0){
-            for (Cliente cliente : this.listaClientes){
-                cliente.toString();
+    public void verListadoClientes() {
+        if (this.listaClientes.size() != 0) {
+            System.out.println("[!] Listado de clientes. Cantidad de clientes: " + this.listaClientes.size());
+            for (int i=0; i<this.listaClientes.size(); i++) {
+                System.out.println("CLiente [" + i + "]");
+                System.out.println(listaClientes.get(i).toString());
             }
-        }
-        else System.out.println("No hay clientes registrados en el sistema.");
+        } else System.out.println("No hay clientes registrados en el sistema.");
     }
 }
