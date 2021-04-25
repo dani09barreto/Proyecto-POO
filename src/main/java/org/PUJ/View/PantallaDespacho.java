aab8a4fd42d342462aec8b5d9aad756ac05cae05
unknown <PROSOFI@041pfin195625.JAVERIANA.LOC>
2021-04-25 13:50:48 -0500

    Añadi objetos en el main

--- src/main/java/org/PUJ/View/PantallaDespacho.java
+++ src/main/java/org/PUJ/View/PantallaDespacho.java
@@ -11,7 +11,61 @@ public class PantallaDespacho {
     private ControlDespacho centralDespacho = new ControlDespacho();
 
     public static void main(String[] args) {
+
         PantallaDespacho pantalla = new PantallaDespacho();
+        //Objetos
+        Cliente cliente1 = new Cliente(1000077617L, "Nicolas David", 316009L, "BG 6447");
+        Cliente cliente2 = new Cliente(1000444324L, "Santiago Gutierrez", 951233L, "SS 1234");
+        Cliente cliente3 = new Cliente(2106226983L, "Daniel Barreto", 443253L, "DD 5678");
+        Cliente cliente4 = new Cliente(1555345340L, "William Martinez", 664323L, "LL 4432");
+        Cliente cliente5 = new Cliente(9931183443L, "Jose Diaz", 221453L, "CL 2224");
+        // 3 de cada uno: Productos - Fruver, aseo, otros
+        Producto producto1 = new Producto("Papas fritas", 2500d, "La esquina");
+        Producto producto2 = new Producto("Yogurt con fresa", 3500d, "La esquina");
+        Producto producto3 = new Producto("Doritos", 4000d, "La otra esquina");
+
+        Aseo productoAseo1 = new Aseo("Escoba", 4500D, "la esquina", "Dersa", TipoProducto.HOGAR, true);
+        Aseo productoAseo2 = new Aseo("Trapero", 7000D, "la esquina", "Dersa", TipoProducto.HOGAR, false);
+        Aseo productoAseo3 = new Aseo("Desengrasante", 12000d, "los arboles", "Wd-40", TipoProducto.INDUSTRIAL, true);
+
+        Fruver productoFruver1 = new Fruver("Manzana", 400D, "los arboles", 0d, "Napoles");
+        Fruver productoFruver2 = new Fruver("Huevos", 1800D, "Justo y bueno", 0d, "Napoles");
+        Fruver productoFruver3 = new Fruver("Arverja", 3000D, "los arboles", 0d, "Quintana");
+        // 1 pedido sin servicio adicional
+        Pedido pedido1 = new Pedido(Calendar.getInstance(), "Camilo", cliente2, producto1);
+        // 5 pedidos con servicios adicionales
+        // - Dos con envio prime y uno con bonoregalo.
+        Pedido pedido2 = new Pedido(Calendar.getInstance(), "Juan", cliente1, producto1);
+        Pedido pedido3 = new Pedido(Calendar.getInstance(), "Miguel", cliente3, producto3);
+        Pedido pedido4 = new Pedido(Calendar.getInstance(), "Nicolas", cliente5, productoAseo1);
+        Pedido pedido5 = new Pedido(Calendar.getInstance(), "Sofia", cliente5, productoFruver1);
+        Pedido pedido6 = new Pedido(Calendar.getInstance(), "Sara", cliente4, productoAseo3);
+
+        pantalla.getCentralDespacho().getGestionCliente().getListaClientes().put(cliente1.getCedula(),cliente1);
+        pantalla.getCentralDespacho().getGestionCliente().getListaClientes().put(cliente2.getCedula(),cliente2);
+        pantalla.getCentralDespacho().getGestionCliente().getListaClientes().put(cliente3.getCedula(),cliente3);
+        pantalla.getCentralDespacho().getGestionCliente().getListaClientes().put(cliente4.getCedula(),cliente4);
+        pantalla.getCentralDespacho().getGestionCliente().getListaClientes().put(cliente5.getCedula(),cliente5);
+
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(producto1.getProdId(),producto1);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(producto2.getProdId(),producto2);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(producto3.getProdId(),producto3);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(productoAseo1.getProdId(),productoAseo1);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(productoAseo2.getProdId(),productoAseo2);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(productoAseo3.getProdId(),productoAseo3);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(productoFruver1.getProdId(),productoFruver1);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(productoFruver2.getProdId(),productoFruver2);
+        pantalla.getCentralDespacho().getGestionProductos().getListaProductos().put(productoFruver3.getProdId(),productoFruver3);
+
+        pantalla.getCentralDespacho().getPedidos().add(pedido1);
+        pantalla.getCentralDespacho().getPedidos().add(pedido2);
+        pantalla.getCentralDespacho().getPedidos().add(pedido3);
+        pantalla.getCentralDespacho().getPedidos().add(pedido4);
+        pantalla.getCentralDespacho().getPedidos().add(pedido5);
+        pantalla.getCentralDespacho().getPedidos().add(pedido6);
+
+        //Fin objetos
+
         int opcion = 0;
         Scanner in = new Scanner(System.in);
 
